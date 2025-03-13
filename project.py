b82d01c01ef99525803d79ac2da0091c7bc6e518
chloechan <chanco@uci.edu>
2025-03-13 11:04:23 -0700

    got the tables copied from HW2

--- project.py
+++ project.py
@@ -1,2 +1,17 @@
-print("hi guys")
-import mysql.connector
+from terminal import * 
+
+
+def main(): 
+
+    first_function = sys.argv[1]
+    UserName = 'test'
+    PassWord = 'password'
+    DataBase_Name = 'cs122a' 
+
+    cur, con = connect(UserName, PassWord, DataBase_Name) 
+    if first_function == 'import': 
+        DB_creation(cur, DataBase_Name)
+        parsefiles(cur, con, sys.argv[2])
+
+if __name__ == '__main__':
+    main()
---
+++ terminal.py
@@ -0,0 +1,117 @@
+import csv
+import sys
+import os
+import mysql.connector
+from mysql.connector.errors import Error
+
+
+def connect(UserName, PassWord, DataBase_Name):
+    try:
+        connection = mysql.connector.connect(user=UserName, password=PassWord, database=DataBase_Name)
+
+        cursor = connection.cursor()
+        return cursor.connection
+    except mysql.connector.Error as e:
+        print("There was an error to connect: ", e)
+        return None, None
+    
+
+
+def DB_creation(cursor, DataBase):
+    cursor.execute('DROP DATABASE IF EXISTS `%s`' %DataBase)
+    cursor.execute('CREATE DATABASE `%s`' %DataBase)
+    cursor.execute('USE `%s`' %DataBase)
+
+    user = """CREATE TABLE Users (
+    uid INT,
+    email TEXT NOT NULL,
+    joined_date DATE NOT NULL,
+    nickname TEXT NOT NULL,
+    street TEXT,
+    city TEXT,
+    state TEXT,
+    zip TEXT,
+    genres TEXT,
+    PRIMARY KEY (uid))"""
+
+    producers = """CREATE TABLE Producers (
+    uid INT,
+    bio TEXT,
+    company TEXT,
+    PRIMARY KEY (uid),
+    FOREIGN KEY (uid) REFERENCES Users(uid) ON DELETE CASCADE)"""
+
+    viewers = """CREATE TABLE Viewers (
+    uid INT,
+    subscription ENUM('free', 'monthly', 'yearly'),
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    PRIMARY KEY (uid),
+    FOREIGN KEY (uid) REFERENCES Users(uid) ON DELETE CASCADE)"""
+
+    releases = """CREATE TABLE Releases (
+    rid INT,
+    producer_uid INT NOT NULL,
+    title TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    release_date DATE NOT NULL,
+    PRIMARY KEY (rid),
+    FOREIGN KEY (producer_uid) REFERENCES Producers(uid) ON DELETE CASCADE)"""
+
+    movies = """CREATE TABLE Movies (
+    rid INT,
+    website_url TEXT,
+    PRIMARY KEY (rid),
+    FOREIGN KEY (rid) REFERENCES Releases(rid) ON DELETE CASCADE)"""
+
+    series = """CREATE TABLE Series (
+    rid INT,
+    introduction TEXT,
+    PRIMARY KEY (rid),
+    FOREIGN KEY (rid) REFERENCES Releases(rid) ON DELETE CASCADE)"""
+
+    videos = """CREATE TABLE Videos (
+    rid INT,
+    ep_num INT NOT NULL,
+    title TEXT NOT NULL,
+    length INT NOT NULL,
+    PRIMARY KEY (rid, ep_num),
+    FOREIGN KEY (rid) REFERENCES Releases(rid) ON DELETE CASCADE)"""
+
+    sessions = """CREATE TABLE sessions (
+    sid INT,
+    uid INT NOT NULL,
+    rid INT NOT NULL,
+    ep_num INT NOT NULL,
+    initiate_at DATETIME NOT NULL,
+    leave_at DATETIME NOT NULL,
+    quality ENUM('480p', '720p', '1080p'),
+    device ENUM('mobile', 'desktop'),
+    PRIMARY KEY (sid),
+    FOREIGN KEY (uid) REFERENCES Viewers(uid) ON DELETE CASCADE,
+    FOREIGN KEY (rid, ep_num) REFERENCES Videos(rid, ep_num) ON DELETE CASCADE)"""
+
+    reviews = """CREATE TABLE Reviews (
+    rvid INT,
+    uid INT NOT NULL,
+    rid INT NOT NULL,
+    rating DECIMAL(2, 1) NOT NULL CHECK (rating BETWEEN 0 AND 5),
+    body TEXT,
+    posted_at DATETIME NOT NULL,
+    PRIMARY KEY (rvid),
+    FOREIGN KEY (uid) REFERENCES Viewers(uid) ON DELETE CASCADE,
+    FOREIGN KEY (rid) REFERENCES Releases(rid) ON DELETE CASCADE)"""
+
+    cursor.execute(user)
+    cursor.execute(producers)
+    cursor.execute(viewers)
+    cursor.execute(releases)
+    cursor.execute(movies)
+    cursor.execute(series)
+    cursor.execute(videos)
+    cursor.execute(sessions)
+    cursor.execute(reviews)
+
+
+def parsefiles():
+    print("hi")
